1e487af2e0b1db9d6a5bfa03a9f95edfe8167644
Lukas <160043122+lukrencijus@users.noreply.github.com>
2024-11-12 13:51:40 +0200

    Update linked_list.s

--- linked_list/linked_list.s
+++ linked_list/linked_list.s
@@ -1,31 +1,61 @@
 .data
-start: .word 0          # Memory pointer for free memory, initially 0
+start: .word 0
+head_node: .word 0
 
 .text
 _start:
-    la t0, start        
-    li t1, 0x80000000    
-    sw t1, 0(t0)         
-
-    addi a0, zero, 0x52 	# 'R' (ASCII 82)
+    addi a0, zero, 0x52 	# 'R' in HEX (ASCII 82)
     jal ra, alloc_node
+    
+    la t0, head_node
+    sw a0, 0(t0)
+    
+    addi a0, zero, 0x56    # 'V' in HEX (ASCII 86)
+    jal ra, alloc_node
+    
+    mv a1, a0
+    
+    la a0, head_node
+    lw a0, 0(a0)
+    jal ra, add_tail
 
 alloc_node:
-    la t0, start         # Address of the `start` pointer
-    lw t1, 0(t0)         # Load the current free memory position
+    la t0, start
+    lw t1, 0(t0)
 
-    addi t2, t1, 12      # Calculate the new memory address
-    sw t2, 0(t0)         # Update the `start` pointer to the new position
+    addi t2, t1, 10
+    sw t2, 0(t0)
+    
+    addi t3, zero, 0xA
+    slli t3, t3, 8
+    or t3, t3, a0
+    
+    # value
+    sh t3, 0(t1)        
 
+    # next
+    sw t1, 2(t1)
 
-    addi t3, zero, 0xA   # MSByte for `val` = 0xA
-    slli t3, t3, 8       # Shift left to set as MSByte
-    or t3, t3, a0        # Combine MSByte with input value in a0
-    sb t3, 0(t1)         # Store the `val` at offset 0 (1 byte)
+    # prev
+    sw t1, 6(t1)
 
-    sw t1, 4(t1)         # Set `next` pointer to itself
+    mv a0, t1
+    jalr zero, ra, 0
+    
+    
+    
+add_tail:
+    mv t0, a0
+find_tail:
+    lw t1, 2(t0)
+    beq t1, t0, set_tail
+    
+    # move to next node
+    mv t0, t1
+    j find_tail
 
-    sw t1, 8(t1)         # Set `prev` pointer to itself
-
-    mv a0, t1            # Return the start address of the node
-    jalr zero, ra, 0     # Return to the caller
+set_tail:
+    sw a1, 2(t0)
+    sw t0, 6(a1)
+    sw a1, 2(a1)    
+    jalr zero, ra, 0
